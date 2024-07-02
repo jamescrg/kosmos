2d7cfdedfbf3aa9697484cb0657355b04bd31b60
tumpa <jtumpa@gmail.com>
2024-07-02 17:34:39 +0200

    refactor: Added separate URL file for contacts app

---
+++ apps/contacts/urls.py
@@ -0,0 +1,43 @@
+from django.urls import path
+
+from apps.contacts.views import (
+    index,
+    select,
+    add,
+    edit,
+    delete,
+    assign,
+    assign_store,
+    remove,
+    remove_store,
+    add_intake,
+    toggle_google_sync,
+    google_list,
+)
+
+urlpatterns = [
+    path("contacts/", index, name="contacts"),
+    path("contacts/<int:id>", select, name="contacts-select"),
+    path("contacts/add", add, name="contacts-add"),
+    path("contacts/<int:id>/edit", edit, name="contacts-edit"),
+    path("contacts/<int:id>/delete", delete, name="contacts-delete"),
+    path("contacts/<int:id>/assign", assign, name="contacts-assign"),
+    path(
+        "contacts/<int:id>/assign/store",
+        assign_store,
+        name="contacts-assign-store",
+    ),
+    path("contacts/<int:id>/remove", remove, name="contacts-remove"),
+    path(
+        "contacts/<int:id>/remove/store",
+        remove_store,
+        name="contacts-remove-store",
+    ),
+    path("contacts/<int:id>/add_intake", add_intake, name="contacts-add-intake"),
+    path(
+        "contacts/<int:id>/toggle_google_sync",
+        toggle_google_sync,
+        name="contacts-toggle-google-sync",
+    ),
+    path("contacts/google_list", google_list, name="contacts-google"),
+]
