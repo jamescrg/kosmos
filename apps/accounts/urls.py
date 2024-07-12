17aed93335026a3f1d2cfc7fdb1f41b80ae65c49
tumpa <jtumpa@gmail.com>
2024-07-12 17:07:02 +0200

    refactor: disable registration

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,10 +1,8 @@
 from django.urls import include, path
 
-from .views import SignUpView
-
 app_name = "accounts"
 
 urlpatterns = [
-    path("signup/", SignUpView.as_view(), name="signup"),
+    # path("signup/", SignUpView.as_view(), name="signup"),
     path("", include("django.contrib.auth.urls")),
 ]
