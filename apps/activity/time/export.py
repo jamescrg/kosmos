572ad95a49540916144b92db8c97e1010a8225d2
James Craig <jamespcraig@gmail.com>
2024-08-24 12:43:37 +0000

    activity, time: two flavors of exports, standard and clio, dropdown menu to choose with confirm

--- apps/activity/time/export.py
+++ apps/activity/time/export.py
@@ -2,6 +2,7 @@ import csv
 
 
 def write_clio_csv(entries, response):
+    entries = entries.exclude(matter__clio_matter_id__isnull=True)
     writer = csv.writer(response)
     writer.writerow(
         [
@@ -16,17 +17,12 @@ def write_clio_csv(entries, response):
             "non-billable",
         ]
     )
-
     for entry in entries:
-
         clio_user = ""
-
         if entry.user.symbol == "JC":
             clio_user = "James Craig"
-
         if entry.user.symbol == "LK":
             clio_user = "Lexi Krier"
-
         writer.writerow(
             [
                 entry.matter.clio_matter_id,
@@ -40,10 +36,40 @@ def write_clio_csv(entries, response):
                 entry.comp,
             ]
         )
-
     return response
 
 
 def write_standard_csv(entries, response):
-    pass
+    writer = csv.writer(response)
+    writer.writerow(
+        [
+            "Date",
+            "Matter",
+            "User",
+            "Actions",
+            "Hours",
+            "Rate",
+            "Fee",
+            "Comp",
+            "Discounted Fee",
+            "Entered",
+            "Invoice",
+        ]
+    )
+    for entry in entries:
+        writer.writerow(
+            [
+                entry.date.strftime("%m/%d/%Y"),
+                entry.matter.name,
+                entry.user.symbol,
+                entry.actions,
+                entry.hours,
+                entry.rate,
+                entry.fee,
+                entry.comp,
+                entry.discounted_fee,
+                entry.entered,
+                entry.invoice,
+            ]
+        )
     return response
