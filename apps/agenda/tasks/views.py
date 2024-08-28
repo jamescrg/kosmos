74bb4122a715734e8edcbdac956c7c9603f92d7f
James Craig <jamespcraig@gmail.com>
2024-08-28 21:26:59 +0000

    consolidated agenda_tasks views into single file

--- apps/agenda/tasks/views.py
+++ apps/agenda/tasks/views.py
@@ -8,7 +8,43 @@ from apps.accounts.models import CustomUser
 from apps.agenda.tasks.filter import TasksFilter
 from apps.agenda.tasks.forms import TaskForm
 from apps.agenda.tasks.models import Task
+from apps.agenda.tasks.tasks import get_table_data
 from apps.matters.models import Matter
+
+
+@login_required
+def index(request):
+    page = "agenda"
+
+    tab = request.session.get("agenda_tab", "tasks")
+    # check whether events have been hidden
+    show_events = request.session.get("show_events", True)
+
+    # if events are hidden, check the date they were hidden
+    # if that date is less than today, show them
+    if not show_events:
+        today = date.today()
+        timestamp = int(request.session.get("hide_expire"))
+        old_date = date.fromtimestamp(timestamp)
+        if today > old_date:
+            show_events = True
+            request.session["show_events"] = True
+
+    table_data = get_table_data(request)
+
+    # save the currently selected matter in the add task form
+    # so multiple tasks can quickly be added to a matter
+    agenda_matter = request.session.get("agenda_matter")
+
+    context = {
+        "tab": tab,
+        "page": page,
+        "show_events": show_events,
+        "agenda_matter": agenda_matter,
+    }
+
+    context = context | table_data
+    return render(request, "agenda/agenda-main.html", context)
 
 
 @login_required
