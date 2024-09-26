5b3c051786bcc4301b5d9cc39a647bd7e683b788
Jan Tumpa <jtumpa@gmail.com>
2024-09-26 20:28:53 +0200

    feat: added comments, refactored dictionaries, added to .env, added function for determining start and end of billing dates

--- apps/billing/invoices/functions/generate_ledes_98b.py
+++ apps/billing/invoices/functions/generate_ledes_98b.py
@@ -1,3 +1,4 @@
+import os
 from itertools import chain
 from tempfile import NamedTemporaryFile
 from typing import Dict, Union
@@ -40,12 +41,30 @@ def _get_combined_entries(invoice: Invoice) -> list:
     """
     Returns a list of combined time and expense entries, with a shared index and sorted by date
     """
+    # Merge time and expense entries into a single iterable
     merged_data = chain(
+        # Create tuples of (entry, type) for each entry in the invoice
         ((entry, "time") for entry in invoice.timeentry_set.all()),
         ((expense, "expense") for expense in invoice.expenseentry_set.all()),
     )
 
+    # Sort the merged data by date in descending order (most recent first)
+    # x[0] refers to the entry object (time or expense entry)
     return sorted(merged_data, key=lambda x: x[0].date, reverse=True)
+
+
+def _get_billing_dates(invoice: Invoice) -> tuple:
+    """
+    Given an invoice, return a billing start and end date
+     - Start date is the earliest date of all entries
+     - End date is the latest date of all entries
+    """
+    combined_entries = _get_combined_entries(invoice)
+
+    earliest_date = min(entry.date for entry, _ in combined_entries)
+    latest_date = max(entry.date for entry, _ in combined_entries)
+
+    return earliest_date, latest_date
 
 
 def _format_line(
@@ -62,40 +81,44 @@ def _format_line(
     timekeeper_name = f"{entry.user.last_name}, {entry.user.first_name}"
     timekeeper_initials = f"{entry.user.first_name[0]}{entry.user.last_name[0]}"
 
+    earliest_date, latest_date = _get_billing_dates(invoice)
+
     common_fields = {
         "INVOICE_DATE": created_at_date,
         "INVOICE_NUMBER": str(invoice.id),
-        "CLIENT_ID": invoice.matter.client_reference_id or "",
-        "LAW_FIRM_MATTER_ID": f"{invoice.matter.name} - {invoice.matter.id}",
+        "CLIENT_ID": str(invoice.matter.client_id),
+        "LAW_FIRM_MATTER_ID": str(invoice.matter_id),
         "INVOICE_TOTAL": str(invoice.value["final_total"]),
-        "BILLING_START_DATE": "20240101",  # NOTE: This date needs to be determined
-        "BILLING_END_DATE": "20240131",  # NOTE: This date needs to be determined
-        "INVOICE_DESCRIPTION": invoice.comment,
+        "BILLING_START_DATE": earliest_date.strftime("%Y%m%d"),
+        "BILLING_END_DATE": latest_date.strftime("%Y%m%d"),
+        "INVOICE_DESCRIPTION": invoice.matter.name,
         "LINE_ITEM_NUMBER": str(index),
     }
 
     if entry_type == "time":
         entry_specific_fields = {
-            "EXP/FEE/INV_ADJ_TYPE": "F",  # NOTE: Confirm if always "F" for time entries
+            "EXP/FEE/INV_ADJ_TYPE": "F",
             "LINE_ITEM_NUMBER_OF_UNITS": str(entry.hours),
-            "LINE_ITEM_ADJUSTMENT_AMOUNT": "0",  # NOTE: Confirm if always 0
+            "LINE_ITEM_ADJUSTMENT_AMOUNT": f"-{entry.fee}" if entry.comp == 1 else "0",
             "LINE_ITEM_TOTAL": str(entry.fee),
             "LINE_ITEM_DATE": entry.date.strftime("%Y%m%d"),
-            "LINE_ITEM_TASK_CODE": "L120",  # NOTE: Confirm if always "L120" for time entries
+            "LINE_ITEM_TASK_CODE": "",
             "LINE_ITEM_EXPENSE_CODE": "",
-            "LINE_ITEM_ACTIVITY_CODE": "A111",  # NOTE: Confirm if always "A111" for time entries
+            "LINE_ITEM_ACTIVITY_CODE": "A111",
             "LINE_ITEM_DESCRIPTION": entry.actions,
             "LINE_ITEM_UNIT_COST": str(entry.rate),
         }
     elif entry_type == "expense":
         entry_specific_fields = {
-            "EXP/FEE/INV_ADJ_TYPE": "E",  # NOTE: Confirm if always "E" for expense entries
+            "EXP/FEE/INV_ADJ_TYPE": "E",
             "LINE_ITEM_NUMBER_OF_UNITS": "1.0",
-            "LINE_ITEM_ADJUSTMENT_AMOUNT": "0",  # NOTE: Confirm if always 0
+            "LINE_ITEM_ADJUSTMENT_AMOUNT": (
+                f"-{entry.amount}" if entry.comp == 1 else "0"
+            ),
             "LINE_ITEM_TOTAL": str(entry.amount),
             "LINE_ITEM_DATE": entry.date.strftime("%Y%m%d"),
             "LINE_ITEM_TASK_CODE": "",
-            "LINE_ITEM_EXPENSE_CODE": "E124",  # NOTE: Confirm if always "E124" for expense entries
+            "LINE_ITEM_EXPENSE_CODE": "",
             "LINE_ITEM_ACTIVITY_CODE": "",
             "LINE_ITEM_DESCRIPTION": entry.description,
             "LINE_ITEM_UNIT_COST": str(entry.amount),
@@ -105,9 +128,9 @@ def _format_line(
 
     common_end_fields = {
         "TIMEKEEPER_ID": timekeeper_initials,
-        "LAW_FIRM_ID": "9395549",  # NOTE: Confirm if this is always the same
+        "LAW_FIRM_ID": os.getenv("LAW_FIRM_ID"),
         "TIMEKEEPER_NAME": timekeeper_name,
-        "TIMEKEEPER_CLASSIFICATION": "PT",  # NOTE: Confirm if always "PT"
+        "TIMEKEEPER_CLASSIFICATION": "PT",
         "CLIENT_MATTER_ID": "[]",
     }
 
