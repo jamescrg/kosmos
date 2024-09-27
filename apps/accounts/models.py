2f2be94a83c3a593058aecc5cac3079d73ee9040
Jan Tumpa <jtumpa@gmail.com>
2024-09-27 17:28:31 +0200

    feat: added the role field to custom user model

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -3,11 +3,17 @@ from django.db import models
 
 from apps.accounts.managers import CustomUserManager
 
+ROLE_OPTIONS = (
+    ("ADMIN", "Admin"),
+    ("USER", "User"),
+)
+
 
 class CustomUser(AbstractUser):
     google_contacts_credentials = models.TextField(null=True, blank=True)
     google_calendar_credentials = models.TextField(null=True, blank=True)
     user_rate = models.IntegerField(default=0)
     initials = models.CharField(max_length=100, null=True, blank=True)
+    role = models.CharField(max_length=5, choices=ROLE_OPTIONS, default="USER")
 
     objects = CustomUserManager()
