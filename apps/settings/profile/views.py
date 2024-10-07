f743f84abaa2363cc5ce9f36bfa0efd086c31fd9
Jan Tumpa <jan@vimlaw.org>
2024-10-07 22:11:16 +0200

    feat: password change in profile

--- apps/settings/profile/views.py
+++ apps/settings/profile/views.py
@@ -2,7 +2,17 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from apps.settings.profile.forms import ProfileForm
+from apps.settings.profile.forms import ChangePasswordForm, ProfileForm
+
+
+def _get_form_errors(form):
+    errors = []
+    for field, error_list in form.errors.items():
+        if field == "__all__":
+            errors.extend(error_list)
+        else:
+            errors.extend(f"{field.capitalize()}: {error}" for error in error_list)
+    return ". ".join(errors)
 
 
 @login_required
@@ -14,21 +24,41 @@ def profile_index(request):
 
 
 @login_required
-def personal_profile(request):
+def personal_profile(request, form_type=None):
     if request.method == "POST":
-        form = ProfileForm(request.POST, instance=request.user)
+        if form_type == "profile":
+            form = ProfileForm(request.POST, instance=request.user)
 
-        if form.is_valid():
-            profile = form.save(commit=False)
-            profile.save()
+            if form.is_valid():
+                profile = form.save(commit=False)
+                profile.save()
 
-            return HttpResponse("Profile updated successfully")
+                return HttpResponse("Profile updated successfully")
+        elif form_type == "password":
+            change_password_form = ChangePasswordForm(
+                request.POST, instance=request.user
+            )
+
+            if change_password_form.is_valid() and form_type == "password":
+                user = change_password_form.save(commit=True)
+                user.save()
+
+                return HttpResponse(
+                    '<div class="success-msg">Password changed successfully</div>'
+                )
+            else:
+                errors = _get_form_errors(change_password_form)
+
+                return HttpResponse(f'<div class="error-msg">{errors}</div>')
+
     else:
         form = ProfileForm(instance=request.user)
+        change_password_form = ChangePasswordForm(instance=request.user)
 
     context = {
         "user": request.user,
         "form": form,
+        "change_password_form": change_password_form,
     }
 
     return render(request, "settings/profile/profile.html", context)
