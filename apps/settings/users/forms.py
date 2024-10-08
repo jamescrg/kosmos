0875b9284b019b23c12d091a3499e102194f02fd
Jan Tumpa <jtumpa@gmail.com>
2024-10-08 17:50:19 +0200

    feat: create user form

--- apps/settings/users/forms.py
+++ apps/settings/users/forms.py
@@ -15,3 +15,25 @@ class UserForm(forms.ModelForm):
             "is_active",
             "initials",
         ]
+
+
+class CreateUserForm(forms.ModelForm):
+    class Meta:
+        model = CustomUser
+        fields = [
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "role",
+            "password",
+        ]
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+
+        if commit:
+            user.save()
+
+        return user
