896b259e3cf160dec8ccaee86414e5cfde41f935
James Craig <jamespcraig@gmail.com>
2024-11-07 12:52:10 +0000

    billing: collection refactor "owed" to "due"

--- apps/billing/collection/views.py
+++ apps/billing/collection/views.py
@@ -11,16 +11,21 @@ def collection_list(request):
 
     # Convert queryset to list so it's sortable by custom properties
     matter_list = list(matters)
-    matter_list.sort(key=lambda x: x.value["invoices"]["owed"], reverse=True)
+    matter_list.sort(key=lambda x: x.value["invoices"]["due"], reverse=True)
 
     page = request.GET.get("page")
     pagination = Paginator(matter_list, 10).get_page(page)
+
+    total_due = 0
+    for matter in matters:
+        total_due += matter.value["invoices"]["due"]
 
     context = {
         "app": "billing",
         "subapp": "collection",
         "matters": pagination.object_list,
         "pagination": pagination,
+        "total_due": total_due,
     }
 
     return render(request, "billing/collection/list.html", context)
