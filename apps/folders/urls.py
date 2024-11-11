8b2738fcfa000cb57045499ff5a02cb8dfe85a04
James Craig <jamespcraig@gmail.com>
2024-11-11 01:59:07 +0000

    folders: revision of folders views and templates

--- apps/folders/urls.py
+++ apps/folders/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 
-from apps.folders.views import delete, edit, insert, select, update
+from apps.folders.views import add, delete, edit, insert, select, update
 
 app_name = "folders"
 
 urlpatterns = [
-    path("folders/<int:id>/<str:app>/<str:action_type>", select, name="list"),
-    path("folders/insert/<str:app>/<str:action_type>", insert, name="insert"),
+    path("folders/<int:folder_id>", select, name="list"),
+    path("folders/add", add, name="add"),
+    path("folders/insert", insert, name="insert"),
     path("folders/edit/<int:folder_id>", edit, name="edit"),
     path("folders/update/<int:folder_id>", update, name="update"),
-    path("folders/delete/<int:folder_id>/<str:app>", delete, name="delete"),
+    path("folders/delete/<int:folder_id>", delete, name="delete"),
 ]
