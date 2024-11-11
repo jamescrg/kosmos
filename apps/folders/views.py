f4036b40ecf96d4404c7070fdb75841bc9b2295b
James Craig <jamespcraig@gmail.com>
2024-11-11 01:59:07 +0000

    folders: revision of folders views and templates

--- apps/folders/views.py
+++ apps/folders/views.py
@@ -1,55 +1,37 @@
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 
+from apps.folders.folders import get_list_data
 from apps.folders.models import Folder
 
 
 @login_required
-def select(request, id, app, action_type=None):
-    if action_type == "db_update":
-        request.session["contacts_selected_folder_id"] = id
+def list(request):
+    context = get_list_data(request)
+    return render(request, "folders/list.html", context)
 
-        if "selected_contact_id" in request.session:
-            del request.session["selected_contact_id"]
 
-        return redirect("contacts:contact-index")
-
-    if app == "contacts":
-        request.session["contacts_selected_folder_id"] = id
-        if "selected_contact_id" in request.session:
-            del request.session["selected_contact_id"]
-
-        return redirect("contacts:contact-index")
-
-    if app == "agenda":
-        folder = get_object_or_404(Folder, pk=id)
-
-        if folder.active == 1:
-            folder.active = 0
-
-        if folder.selected == 1:
-            folder.selected = 0
-        else:
-            folder.selected = 1
-
-        folder.save()
-
+@login_required
+def select(request, folder_id):
+    request.session["contacts_selected_folder_id"] = folder_id
     return redirect("contacts:contact-index")
 
 
 @login_required
-def insert(request, app, action_type=None):
+def add(request):
+    return render(request, "folders/add.html")
+
+
+@login_required
+def insert(request):
     folder = Folder()
     folder.user_id = request.user.id
-    folder.app = app
     folder.name = request.POST["name"]
-    folder.save()
-
-    if action_type == "db_update":
-        return redirect("contacts:add")
-
-    return HttpResponse(status=204, headers={"HX-Trigger": "contactsChanged"})
+    folder.app = "contacts"
+    if folder.name:
+        folder.save()
+    context = get_list_data(request)
+    return render(request, "folders/list.html", context)
 
 
 @login_required
@@ -64,18 +46,17 @@ def update(request, folder_id):
     folder = get_object_or_404(Folder, pk=folder_id)
     folder.name = request.POST["name"]
     folder.save()
-    context = {"app": "contacts", "folder": folder}
+    context = {"folder": folder}
     return render(request, "folders/folder.html", context)
 
 
 @login_required
-def delete(request, folder_id, app):
-    folder = get_object_or_404(Folder, pk=id)
+def delete(request, folder_id):
+    folder = get_object_or_404(Folder, pk=folder_id)
     folder.delete()
 
-    if request.session.get("contacts_selected_folder_id") == id:
+    if request.session.get("contacts_selected_folder_id") == folder_id:
         del request.session["contacts_selected_folder_id"]
 
-    folders = Folder.objects.filter(app=app).order_by("name")
-    context = {"folders": folders}
+    context = get_list_data(request)
     return render(request, "folders/list.html", context)
