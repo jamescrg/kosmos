ae609595f3bf18bdce5f8b5c705e643901532815
James Craig <jamespcraig@gmail.com>
2025-05-21 21:19:24 +0000

    agenda: tasks: upgraded admin interface with django-unfold

--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,7 @@ ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 # Application definition
 
 INSTALLED_APPS = [
+    "unfold",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
