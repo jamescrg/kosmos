85ceafcb59ccc59a4240e22ace62f83b5cd6aa75
jan <72min@proton.me>
2025-08-04 20:49:17 +0200

    KAN-251 Agenda Tasks (#301)
    * feat: KAN-252 default ordering
    
    - Added priority as default ordering when opening taskṣ
    
    * feat: KAN-253 filtering by focus
    
    - Added functionality to filter by the focus field on the tasks table
     - Added order/reoder logic to sorting on tasks
    
    * feat: KAN-254 bootstrap dropdown
    
    - Replaced native select elements on task list to bootstrap dropdowns
    
    * feat: KAN-256 matter name dropdown

--- apps/agenda/tasks/tasks.py
+++ apps/agenda/tasks/tasks.py
@@ -14,24 +14,34 @@ def get_list_data(request):
     filter_data = request.session.get("tasks_filter", {})
 
     if filter_data:
+        filter_data = {
+            **filter_data,
+            "order_by": filter_data.get("order_by", "-priority"),
+        }
+
         filter = TasksFilter(filter_data)
         tasks = filter.qs
+
         user_id = filter_data.get("user")
         user_id = int(user_id) if user_id not in (None, "") else None
+
         matter_id = filter_data.get("matter")
         matter_id = int(matter_id) if matter_id not in (None, "") else None
+
         focus = filter_data.get("focus")
 
     else:
         default_filter = {
             "status": "Pending",
             "matter": None,
-            "order_by": "priority",
+            "order_by": "-priority",
             "user": request.user.id,
             "focus": "",
         }
+
         filter = TasksFilter(default_filter)
         tasks = filter.qs
+
         user_id = request.user.id
         matter_id = None
         focus = ""
@@ -39,6 +49,14 @@ def get_list_data(request):
     pagination = CustomPaginator(
         tasks, per_page=20, request=request, session_key="tasks_pagination"
     )
+
+    selected_matter = None
+    if matter_id:
+        selected_matter = Matter.objects.filter(id=matter_id).first()
+
+    selected_user = None
+    if user_id:
+        selected_user = CustomUser.objects.filter(id=user_id).first()
 
     list_data = {
         "pagination": pagination,
@@ -50,6 +68,8 @@ def get_list_data(request):
         "users": CustomUser.objects.filter(is_active=True).order_by("username"),
         "user_id": user_id,
         "matter_id": matter_id,
+        "selected_matter": selected_matter.name if selected_matter else None,
+        "selected_user": selected_user.username.capitalize() if selected_user else None,
         "focus": focus,
         "filter_label": filter_data.get("filter_label", None) if filter_data else None,
     }
