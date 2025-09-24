223a20d1cb59f5dad48142fe94f4ec9ab48b03c7
Jan Tumpa <jan@vimlaw.org>
2025-09-24 20:41:27 +0200

    fix: remove preserved files on close
    - Remove preserved dropzone files on form modal close

--- static/js/file-upload-forms.js
+++ static/js/file-upload-forms.js
@@ -266,5 +266,6 @@ document.body.addEventListener("htmx:afterSwap", () => {
 
 // Clear preserved files when modal is closed/hidden
 document.body.addEventListener("hidden.bs.modal", () => {
+  documentDropzone = null;
   preservedDropzoneFiles = [];
 });
