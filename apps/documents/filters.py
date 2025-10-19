596c7462e6c1d0dd63b5aaa615950fe96c787957
jan <72min@proton.me>
2025-10-19 16:30:15 +0200

    KAN-280/310-labels&bulk (#328)
    * feat: label add/edit/display/sort/filter
    
    * feat: form changes & label delete
    
    * feat: checkbox selection & label render & label filter
    
    * feat: add and edit label saving
    
    * feat: proceeding and label dynamic switching
    
    * fix: import
    
    * feat: initial bulk updating
    
    * feat: bulk update complete

--- apps/documents/filters.py
+++ apps/documents/filters.py
@@ -1,6 +1,6 @@
 import django_filters
 
-from apps.documents.models import Document
+from apps.documents.models import Document, Label
 from apps.matters.models import Matter
 
 
@@ -11,6 +11,13 @@ class DocumentsFilter(django_filters.FilterSet):
         .distinct()
         .order_by("name"),
         empty_label="All",
+    )
+    label = django_filters.ModelChoiceFilter(
+        queryset=Label.objects.filter(documents__isnull=False)
+        .distinct()
+        .order_by("name"),
+        empty_label="All",
+        field_name="labels",
     )
     order_by = django_filters.OrderingFilter(
         fields=[
@@ -24,4 +31,24 @@ class DocumentsFilter(django_filters.FilterSet):
 
     class Meta:
         model = Document
+        fields = ["name", "matter", "label", "order_by"]
+
+
+class LabelsFilter(django_filters.FilterSet):
+    name = django_filters.CharFilter(lookup_expr="icontains", label="Name")
+    matter = django_filters.ModelChoiceFilter(
+        queryset=Matter.objects.filter(labels__isnull=False)
+        .distinct()
+        .order_by("name"),
+        empty_label="All",
+    )
+    order_by = django_filters.OrderingFilter(
+        fields=[
+            ("name", "name"),
+            ("matter__name", "matter"),
+        ]
+    )
+
+    class Meta:
+        model = Label
         fields = ["name", "matter", "order_by"]
